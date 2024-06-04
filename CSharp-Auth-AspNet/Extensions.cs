438c866056427385c6b8fd88ee6d966899ceeff3
jgmamxmn <65511890+jgmamxmn@users.noreply.github.com>
2024-06-04 14:02:39 +0200

    Added explicit session support to AspNetCore extension

--- CSharp-Auth-AspNet/Extensions.cs
+++ CSharp-Auth-AspNet/Extensions.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Http;
 using Delight.Shim;
 using Delight.Auth;
 using System.Runtime.CompilerServices;
+//using CSharp_Auth_AspNet;
 
 namespace Delight.Shim.AspNetCore
 {
@@ -18,12 +19,40 @@ namespace Delight.Shim.AspNetCore
 		/// <param name="AB"></param>
 		/// <param name="Ctx"></param>
 		/// <returns></returns>
-		public static Delight.Auth.Auth.AuthBuilder SetEnvironment(this Delight.Auth.Auth.AuthBuilder AB, Microsoft.AspNetCore.Http.HttpContext Ctx)
+		[MethodImpl(MethodImplOptions.NoOptimization)]
+		public static Delight.Auth.Auth.AuthBuilder SetEnvironment(this Delight.Auth.Auth.AuthBuilder AB, Microsoft.AspNetCore.Http.HttpContext Ctx, bool createSession)
 		{
 			AB.SetCookieManager((_PhpInstance) => new AspNetCookieMgr(_PhpInstance, Ctx));
 			AB.SetClientIp(Ctx.Request.GetRemoteIp());
+
+			if (createSession)
+			{
+				var bCookie = Ctx.Request.Cookies.TryGetValue(SessionIdCookieName, out string sessId);
+				_SESSION outSess = null;
+				var bSess = (bCookie ? ActiveSessions.TryGetValue(sessId, out outSess) : false);
+				if (bSess)
+				{
+					AB.SetSessionManager(outSess);
+				}
+				else
+				{
+					AB.SetSessionManager((_PhpInstance) =>
+					{
+						var newSess = new _SESSION();
+						var newSessId = Guid.NewGuid().ToString();
+						ActiveSessions.Add(newSessId, newSess);
+						var newCookie = new Cookie.Cookie(SessionIdCookieName, _PhpInstance);
+						newCookie.setValue(newSessId);
+						_PhpInstance._COOKIE.Set(SessionIdCookieName, newCookie);
+						return newSess;
+					});
+				}
+			}
+
 			return AB;
 		}
+		private static Dictionary<string, Delight.Shim._SESSION> ActiveSessions = new Dictionary<string, Delight.Shim._SESSION>();
+		private const string SessionIdCookieName= "CSAuthSessionId";
 
 		public static System.Net.IPAddress GetRemoteIp(this Microsoft.AspNetCore.Http.HttpRequest Request)
 		{
